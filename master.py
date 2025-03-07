17db715fb44212500a4647e489339e7a9a5bbe87
michaelrossherron <michaelrossherron@gmail.com>
2025-03-07 12:34:06 -0700

    Added workflow

--- master.py
+++ master.py
@@ -1,21 +1,20 @@
 import subprocess, os, shutil, glob
-# from buildManual import buildMan 
 
 # Clone the FEBio and FEBioStudio repositories
-FEBIO_GIT = "https://github.com/febiosoftware/FEBio.git"
 FBS_GIT = "https://github.com/febiosoftware/FEBioStudio.git"
+FEBIO_GIT = "https://github.com/febiosoftware/FEBio.git"
 
-subprocess.call(["git", "clone", "--branch", "master", "--depth", "1", FEBIO_GIT])
 subprocess.call(["git", "clone", "--branch", "master", "--depth", "1", FBS_GIT])
+subprocess.call(["git", "clone", "--branch", "master", "--depth", "1", FEBIO_GIT])
 
 # Get the version numbers
-out = subprocess.check_output(["git", "-C", "FEBio", "describe"]).replace("v", "").split('.')
-FEBIO_MAJOR = out[0].strip()
-FEBIO_MINOR = out[1].strip()
-
 out = subprocess.check_output(["git", "-C", "FEBioStudio", "describe"]).replace("v", "").split('.')
 FBS_MAJOR = out[0].strip()
 FBS_MINOR = out[1].strip()
+
+out = subprocess.check_output(["git", "-C", "FEBio", "describe"]).replace("v", "").split('.')
+FEBIO_MAJOR = out[0].strip()
+FEBIO_MINOR = out[1].strip()
 
 # Set up the directory structure for lyx to html conversion
 BASEDIR = os.getcwd() + "/"
@@ -30,7 +29,7 @@ os.mkdir(FU_LYX_DIR)
 os.mkdir(FT_LYX_DIR)
 
 shutil.copy("FEBioStudio/Documentation/FEBioStudio_User_Manual.lyx", FBS_LYX_DIR)
-shutil.copy("FEBioStudio/Documentation/FEBioStudio.bib", FBS_LYX_DIR)
+# shutil.copy("FEBioStudio/Documentation/FEBioStudio.bib", FBS_LYX_DIR)
 shutil.copytree("FEBioStudio/Documentation/Figures", FBS_LYX_DIR + "Figures")
 
 shutil.copy("FEBio/Documentation/FEBio_User_Manual.lyx", FU_LYX_DIR)
@@ -66,7 +65,7 @@ def copyHTMLFiles(srcDir, destDir):
         shutil.copy(file, destDir)
 
 shutil.copytree(JEKYLL_TEMPLATE_DIR, FBS_JEKYLL_DIR)
-shutil.copy(FBS_LYX_DIR + "FEBioStudio.bib", FBS_JEKYLL_DIR)
+# shutil.copy(FBS_LYX_DIR + "FEBioStudio.bib", FBS_JEKYLL_DIR)
 shutil.copytree(FBS_LYX_DIR + "Figures", FBS_JEKYLL_DIR + "Figures")
 shutil.copy(BASEDIR + "febioLogo.png", FBS_JEKYLL_DIR + "Figures")
 shutil.copy(BASEDIR + "FBSIndex.md", FBS_JEKYLL_DIR + "index.md")
@@ -110,12 +109,10 @@ editIndexFile(FT_JEKYLL_DIR + "index.md")
 def runJekyll(jekyllDir, baseURL):
     os.chdir(jekyllDir)
     os.system("bundle install")
-    os.system("bundle exec jekyll serve -b" + baseURL)
+    os.system("bundle exec jekyll build -b" + baseURL)
 
-runJekyll(FBS_JEKYLL_DIR, "/")
-# runJekyll(FU_JEKYLL_DIR, "/")
-# runJekyll(FT_JEKYLL_DIR, "/")
+runJekyll(FBS_JEKYLL_DIR, "/docs/FEBioStudio-" +  FBS_MAJOR + "-" + FBS_MINOR)
+runJekyll(FU_JEKYLL_DIR, "/docs/FEBioUser-" + FEBIO_MAJOR + "-" + FEBIO_MINOR)
+runJekyll(FT_JEKYLL_DIR, "/docs/FEBioTheory-" + FEBIO_MAJOR + "-" + FEBIO_MINOR)
 
-# runJekyll(FBS_JEKYLL_DIR, "/docs/FEBioStudio-" +  FBS_MAJOR + "-" + FBS_MINOR)
-# runJekyll(FU_JEKYLL_DIR, "/docs/FEBioUser-" + FEBIO_MAJOR + "-" + FEBIO_MINOR)
-# runJekyll(FT_JEKYLL_DIR, "/docs/FEBioTheory-" + FEBIO_MAJOR + "-" + FEBIO_MINOR)
+os.chdir(BASEDIR)
